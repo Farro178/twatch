a67ce70f087f2b1018e6288c5363dfb8ca3ed775
Farro178 <jamesfarrelly178@gmail.com>
2021-08-05 17:45:40 +0200

    Implementing redux forms

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,7 +1,53 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
-};
+class StreamCreate extends React.Component {
+  renderInput({ input, label }) {
+    return (
+      // previously had onChange={formProps.input.onChange} value={formProps.input.value} in input but {...formProps.input} does the same but more
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} />
+      </div>
+    );
+  }
 
-export default StreamCreate;
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
+
+  validate = (formValues) => {
+    const errors = {};
+
+    if (!formValues.title) {
+      errors.title = "You must enter a title";
+    }
+
+    if (!formValues.description) {
+      errors.description = "You must enter a description";
+    }
+
+    return errors;
+  };
+
+  render() {
+    return (
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className="ui form"
+      >
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter Description"
+        />
+        <button className="ui button primary">Submit</button>
+      </form>
+    );
+  }
+}
+
+export default reduxForm({
+  form: "streamCreate",
+})(StreamCreate);
